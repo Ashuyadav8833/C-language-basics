d36549f2e8008f7d20abd8b3761e29c2e192c563
Ashuyadav8833 <ashu8833yadav@gmail.com>
2025-03-17 21:25:35 +0530

    Create variables.c

---
+++ Beginner/variables.c
@@ -0,0 +1,20 @@
+// Variables and Data Types in C
+// This program demonstrates the use of different variable types in C.
+
+#include <stdio.h>  // Standard Input-Output library
+
+int main() {
+    // Declaring different types of variables
+    int age = 21;             // Integer type variable
+    float pi = 3.1415;        // Floating-point variable
+    char grade = 'A';         // Character variable
+    double largeNumber = 1.23456789; // Double precision floating-point variable
+
+    // Printing the values of variables
+    printf("Age: %d\n", age);               // %d is used for integers
+    printf("Value of Pi: %f\n", pi);        // %f is used for float
+    printf("Grade: %c\n", grade);           // %c is used for characters
+    printf("Large Number: %lf\n", largeNumber); // %lf is used for double
+
+    return 0;  // Returning 0 indicates successful execution
+}
